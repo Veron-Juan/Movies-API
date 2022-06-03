15ea2519c4100727c898cd93a98f1c49130e39a9
Juan Veron <juan_juve97@hotmail.com>
2022-06-03 19:55:48 -0300

    echo la parte de tranding movies

--- src/main.js
+++ src/main.js
@@ -1,1 +1,26 @@
-console.log('hola')
+async function getTrendingMoviesPreview(){
+    const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
+    const data = await res.json();
+
+    const movies = data.results;
+    movies.forEach(movie => {
+        const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList')
+        const movieContainer = document.createElement('div');
+        //lo q se hace aca es que al movieContainer se le agrega una clase ya existente
+        movieContainer.classList.add('movie-container')
+
+        const movieImg = document.createElement('img');
+        movieImg.classList.add('movie-img');
+        // acá con setAtributte agregamos la etiqueta alt, y como segundo parametro lo que va adentro, en el objeto data aparece como movie.title cada titulo de la pelicula
+        movieImg.setAttribute('alt', movie.title)
+        movieImg.setAttribute(
+            'src', 
+            'https://image.tmdb.org/t/p/w300' + movie.poster_path);
+
+        movieContainer.appendChild(movieImg);
+        trendingPreviewMoviesContainer.appendChild(movieContainer);
+
+    })
+}
+
+getTrendingMoviesPreview();
